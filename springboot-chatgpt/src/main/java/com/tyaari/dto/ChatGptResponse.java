c38b966a7a0f3d373fd80891e68c7788a351a17c
abdulrashid06 <115461514+abdulrashid06@users.noreply.github.com>
2023-07-28 15:52:58 +0530

    Add files via upload

---
+++ springboot-chatgpt/src/main/java/com/tyaari/dto/ChatGptResponse.java
@@ -0,0 +1,18 @@
+package com.tyaari.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChatGptResponse {
+
+    private List<Choice> choices;
+
+    
+
+}
